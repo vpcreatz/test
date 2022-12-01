d82e88de5adf24d0f37fab15e456802ac6c6bea1
test00123 <109013696+test00123@users.noreply.github.com>
2022-12-01 20:36:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ def humanbytes(size):
 
 shortz = shortzy.Shortzy(SHORTENER_API, "shareus.in")
 async def get_shortlink(link):
-   If SHORTENER_API:
+   if SHORTENER_API:
        if LONG_DROPLINK_URL =="True" or LONG_DROPLINK_URL is True:
           return await shortz.get_quick_link(link)
        else:
